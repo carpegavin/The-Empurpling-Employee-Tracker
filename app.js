70605de9e46821e66aba96bd13419926f585ffa0
Gavin Phill <carpe.gavin@gmail.com>
2021-01-08 17:10:19 -0600

    hardcoded sql inserts into table to test.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 // const {} = require('./lib/const');
 
-const {promptDepartment, promptRole, promptFirstName, promptLastName, promptSalary, promptManager} = require('./lib/prompts');
+const {mainMenu, promptDepartment, promptRole, promptFirstName, promptLastName, promptSalary, promptManager} = require('./lib/prompts');
 
-const {departmentData, roleData, firstNameData, lastNameData, salaryData, managerData} = require('./lib/queries');
+const {departmentData, roleData, newDepartment, firstNameData, lastNameData, salaryData, managerData} = require('./lib/queries');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -17,60 +17,48 @@ const connection = mysql.createConnection({
 connection.connect(async (err) => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    await 
-    promptDepartment(),
-    await
-    promptRole(),
-    await
-    promptFirstName(),
-    await
-    promptLastName(),
-    await
-    promptSalary();
-    await
-    promptManager();
-    console.table();
+    // await 
+    // promptDepartment(),
+    // await
+    // promptRole(),
+    // await
+    // promptFirstName(),
+    // await
+    // promptLastName(),
+    // await
+    // promptSalary();
+    // await
+    // promptManager();
+    // console.table();
+    init();
     // connection.end();
 });
 
-module.exports = {
-departmentData,
-roleData,
-firstNameData,
-lastNameData,
-salaryData,
-managerData
-};
+// module.exports = {
+// departmentData,
+// roleData,
+// firstNameData,
+// lastNameData,
+// salaryData,
+// managerData
+// };
 
-// async function runSearch() {
+async function init() {
+    let response = await mainMenu()
+    if(response.mainChoice == "View Departments"){
+        departmentData(connection)
+        init();
+    }else if(response.mainChoice == "View Roles") {
+        roleData(connection)
+        init();
+    }else if (response.mainChoice == "Add Departments"){
+        let newDept = await promptDepartment()
+        console.log(newDept.department);
+        newDepartment(connection, newDept.department)
+        init();
+    }
+}
 
-//     let answer, artist, range;
 
-//     answer = await promptDepartment();
 
-//     switch (answer.name) {
-//         case ALL_SONGS_BY_ARTIST:
-//             artist = await promptArtistName();
-//             artistSearch(connection, artist.name);
-//             break;
 
-//         case ARTISTS_WITH_TWO_OR_MORE_SONGS:
-//             multiSearch(connection);
-//             break;
-
-//         case SONGS_BY_RANGE:
-//             range = await promptRange();
-//             rangeSearch(connection, range.start, range.end);
-//             break;
-
-//         case SPECIFIC_SONG:
-//             song = await promptSong();
-//             songSearch(connection,song.title);
-//             break;
-
-//         case TOP_SONG_AND_ALBUM_BY_ARTIST:
-//             artist = await promptArtistName();
-//             songAndAlbumSearch(connection, artist.name);
-//             break;
-//     };
-// }
