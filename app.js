8ca22c797b96fb8480dec4fbe0bf670f9c5ea8f2
Gavin Phill <carpe.gavin@gmail.com>
2021-01-08 19:47:21 -0600

    got the prompts for salary, manager, and role to work; however, after user input I am getting an undefined error

--- app.js
+++ app.js
@@ -30,32 +30,32 @@ async function init() {
     }else if(response.mainChoice == "View Employees") {
         employeeData(connection)
          init();
-    }else if (response.mainChoice == "Add Departments"){
+    }else if(response.mainChoice == "Add Departments"){
         let newDept = await promptDepartment()
         console.log(newDept.department);
         newDepartment(connection, newDept.department)
         init();
-    }else if (response.mainChoice == "Add Roles"){
+    }else if(response.mainChoice == "Add roles"){
         let newRole = await promptRole()
         console.log(newRoles.role);
         newRole(connection, newRoles.role)
          init();
-    }else if (response.mainChoice == "Add employee first name"){
+    }else if(response.mainChoice == "Add employee first name"){
         let newFirst = await promptFirstName()
         console.log(newFirst.employee);
         newFirstName(connection, newFirst.employee)
         init();
-    }else if (response.mainChoice == "Add employee last name"){
+    }else if(response.mainChoice == "Add employee last name"){
         let newLast = await promptLastName()
         console.log(newLast.employee);
         newLastName(connection, newLast.employee)
         init();
-    }else if (response.mainChoice == "Add employee salary"){
+    }else if(response.mainChoice == "Add salary"){
         let newSal = await promptSalary()
         console.log(newSal.employee);
         newSalary(connection, newSal.employee)
         init();
-    }else if (response.mainChoice == "Add Manager"){
+    }else if(response.mainChoice == "Add Manager"){
         let newMan = await promptManager()
         console.log(newMan.manager);
         newManager(connection, newMan.manager)
